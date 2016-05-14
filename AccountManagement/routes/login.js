2338d88ff3f46504c2b2dae521131fb21b037664
wqc0712 <wqc0712@qq.com>
2016-05-14 18:35:39 +0800

    登录及注册模块数据库交互

--- AccountManagement/routes/login.js
+++ AccountManagement/routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var  client = require('../database');
 
 var mysql = client.getDbCon();
-
+var success = 'false';
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('login',{title : 'Express'});
@@ -25,15 +25,24 @@ router.route('/')
             throw err;
         } else {
             //返回用户id
-            return callback(err, fields);
+            console.log(results[0]);
+            console.log(req.body);
+            if (results[0] === undefined) {
+            	//req.flash('error', ' 用户不存在'); 
+            	res.redirect('login');
+            } else {
+            	if (req.body.Loginpass === results[0].Password) {
+            		console.log(req.body.Loginpass);
+            		req.session.user=req.body.Loginuser;
+            		res.redirect('/');
+            	} else {
+            		//req.flash('error', ' 密码'); 
+            		res.redirect('login');
+            	}
+            }
         }
     });
-    consolt.log(results);
-    if(req.body.Loginuser === user.username && req.body.Loginpass === user.password){
-    	req.session.user=req.body.Loginuser;
-        res.redirect('/');
-    }
-    res.redirect('login');
+  
 });
 
 module.exports = router;
--- AccountManagement/routes/register.js
+++ AccountManagement/routes/register.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var  client = require('../database');
+var mysql = client.getDbCon();
+var  uid = require('../utils/uuid');//用于生成id
 
+var uuid = uid.v4();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('register',{title : 'Express'});
@@ -11,13 +15,23 @@ router.route('/')
     res.render('register', { title: '用户注册' });
 })
 .post(function(req, res) {
-    var user={
-        username: 'admin',
-        password: '123456'
+    if(req.body['Regrepass']!=req.body['Regpass']){
+        return res.redirect('register');
     }
-    if(req.body.Loginuser === user.username && req.body.Loginpass === user.password){
-        res.redirect('/');
+    var sql ="insert into UserAccount (AccountID,AccountName,Password,Phone,Email,State,IdNo) values(?,?,?,?,?,?,?)";
+    if (req.body.Buy) {
+        state = 0;
+    } else {
+        state = 1;
     }
+    mysql.query(sql,[uuid,req.body.Reguser,req.body.Regpass,req.body.RegPhone,req.body.Regemail,state,req.body.RegId],function(err,results,fields){
+        if (err) {
+            throw err;
+        } else {
+            //返回用户id
+            //return callback(err, uuid, fields);
+        }
+    });
     res.redirect('login');
 });
 
