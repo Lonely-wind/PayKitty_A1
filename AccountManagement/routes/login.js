a8632fc4d62e8de12e8736e7b48e5b76a1bc6f5a
wqc0712 <wqc0712@qq.com>
2016-05-14 16:55:48 +0800

    Update

--- AccountManagement/routes/login.js
+++ AccountManagement/routes/login.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var  client = require('../database');
+
+var mysql = client.getDbCon();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -15,6 +18,17 @@ router.route('/')
         username: 'admin',
         password: '123456'
     }
+    var sql = "select * from UserAccount where AccountName = '" + req.body.Loginuser + "';";
+    console.log(sql);
+    mysql.query(sql,function(err,results,fields){
+        if (err) {
+            throw err;
+        } else {
+            //返回用户id
+            return callback(err, fields);
+        }
+    });
+    consolt.log(results);
     if(req.body.Loginuser === user.username && req.body.Loginpass === user.password){
     	req.session.user=req.body.Loginuser;
         res.redirect('/');
