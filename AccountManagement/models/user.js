407089f8d3e286ec76b13f89d2c694b7c13d51e2
wqc0712 <wqc0712@qq.com>
2016-05-17 15:56:44 +0800

    修改register数据库操作，进行手机号、邮箱、身份证号正则检查

--- AccountManagement/models/user.js
+++ AccountManagement/models/user.js
@@ -3,32 +3,40 @@ var  uid = require('../utils/uuid');//用于生成id
 function  User(user) {
     this.name = user.name;
     this.password = user.password;
+    this.phone = user.phone;
+    this.email = user.email;
+    this.Type = user.Type;
+    this.IdNo = user.IdNo;
 }
-var tableName = "user";
+var tableName = "UserAccount";
 mysql = client.getDbCon();
 module.exports = User;
 //新增用户
-/*
+
 User.prototype.save = function  save(callback) {
     // 用户对象
     var  user = {
         name: this.name,
-        password: this.password
+        password: this.password,
+        phone: this.phone,
+        email: this.email,
+        Type: this.Type,
+        IdNo: this.IdNo
     };
-    uuid = uid.v4();
+    //uuid = uid.v4();
     //插入数据库
-    var sql ="insert into user (id,name,password) values(?,?,?)";
+    var sql ="insert into UserAccount (AccountName,Password,Phone,Email,Type,IdNo) values(?,?,?,?,?,?)";
 
-    mysql.query(sql,[uuid,user.name,user.password],function(err,results,fields){
+    mysql.query(sql,[user.name,user.password,user.phone,user.email,user.Type,user.IdNo],function(err,results,fields){
         if (err) {
             throw err;
         } else {
             //返回用户id
-            return callback(err, uuid, fields);
+            return callback(err, user.IdNo, fields);
         }
     });
 };
-*/
+
 
 //获取用户
 /*
--- AccountManagement/routes/register.js
+++ AccountManagement/routes/register.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var  client = require('../database');
 var mysql = client.getDbCon();
 var  uid = require('../utils/uuid');//用于生成id
+var User = require('../models/user');
 
 var uuid = uid.v4();
 /* GET users listing. */
@@ -16,89 +17,52 @@ router.route('/')
 })
 .post(function(req, res) {
     if(req.body['Regrepass']!=req.body['Regpass']){
+        console.log("Different Password!");
         return res.redirect('register');
     }
 
-    /*var result;
-    result =  function  save(callback) {
-        var sql = "select * from UserAccount where AccountName = '" + req.body.Reguser + "';";
-        mysql.query(sql,function(err,results,fields){
-        if (err) {
-            throw err;
-        } else {
-            if (results[0] != undefined) {
-                //req.flash('error', ' 用户不存在'); 
-                callback(err,results[0],fields);
-            }
-        }
-        })
-    };
-    console.log(result);
-    if (result != undefined) {
+    var emailtest = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (!emailtest.test(req.body.Regemail)) {
+        console.log('Wrong Email!');
         return res.redirect('register');
     }
 
-    result =  function  save(callback) {
-        var sql = "select * from UserAccount where Email = '" + req.body.Regemail + "';";
-        mysql.query(sql,function(err,results,fields){
-        if (err) {
-            throw err;
-        } else {
-            if (results[0] != undefined) {
-                //req.flash('error', ' 用户不存在'); 
-                callback(err,results[0],fields);
-            }
-        }
-        })
-    };
-    console.log(result);
-    if (result != undefined) {
+    var phonetest = /^(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/;
+    if (!phonetest.test(req.body.Regmobi)) {
+        console.log('Wrong Mobile Phone Number!');
         return res.redirect('register');
     }
 
-    result =  function  save(callback) {
-        var sql = "select * from UserAccount where Phone = '" + req.body.Regmobi + "';";
-        mysql.query(sql,function(err,results,fields){
-        if (err) {
-            throw err;
-        } else {
-            if (results[0] != undefined) {
-                //req.flash('error', ' 用户不存在'); 
-                callback(err,results[0],fields);
-            }
-        }
-        })
-    };
-    console.log(result);
-    if (result != undefined) {
-        return res.redirect('register');
-    }    
-    */
-
-/*  if (req.body.RegId.length != 18) {
-        //输出身份证错误信息
+    var idtest = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
+    if (!idtest.test(req.body.Regid)) {
+        console.log('Wrong Id Number!');
         return res.redirect('register');
     }
-    if (req.body.RegPhone.length != 11) {
-        //输出电话错误信息
-        return res.redirect('register');
-    }*/
-
-    sql ="insert into UserAccount (AccountID,AccountName,Password,Phone,Email,Type,IdNo) values(?,?,?,?,?,?,?)";
 
     if (req.body.Buy === 'on') {
         state = 0;
     } else {
         state = 1;
     }
-    mysql.query(sql,[uuid,req.body.Reguser,req.body.Regpass,req.body.RegPhone,req.body.Regemail,state,req.body.RegId],function(err,results,fields){
-        if (err) {
-            throw err;
-        } else {
-            req.session.user=req.body.Reguser;
-            res.redirect('/');
-        }
+
+    var  newUser =  new  User({ 
+        name: req.body.Reguser, 
+        password: req.body.Regpass,
+        phone: req.body.Regmobi,
+        email: req.body.Regemail,
+        Type: state,
+        IdNo: req.body.Regid
+    }); 
+
+    newUser.save(function (err) { 
+        if (err) { 
+             return  res.redirect('register'); 
+         } 
+         newUser.password = 'NotTellingYou!';
+        req.session.user = newUser; 
+        res.redirect('account'); 
     });
+
 });
 
 module.exports = router;
