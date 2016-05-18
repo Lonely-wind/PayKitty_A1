925f3cf8e76bd7106405ce3f3f6fcc1685d9d3c4
xixilemon <youweilemon@163.com>
2016-05-18 22:22:26 +0800

    Add files via upload

---
+++ AccountManagement/models/getapi.js
@@ -0,0 +1,32 @@
+var url = require('url');
+var querystring = require('querystring');
+var http = require('http');
+var GetApi = function GetApi(regUrl, postData,  callback){
+    var post_option = url.parse(regUrl);
+    post_option.method = 'POST';
+    post_option.port = 3000;
+    var post_data = querystring.stringify(postData);
+    post_option.headers = {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Content-Length' : post_data.length,
+    };
+    var responseData = '';
+    var x = '';
+    var post_req = http.request(post_option, function(res){
+        res.setEncoding('utf8');
+        res.on('data', function(chunk){
+            responseData += chunk;
+        }).on('end', function() {
+            x = JSON.parse(responseData);
+            for (var i = x.length - 1; i >= 0; i--) {
+                console.log(x[i].num + "\t" + x[i].seller + "\t" + x[i].money + "\t" + x[i].state + "\n");
+            };
+            callback(x);
+        })
+    }).on('error', function(e){
+        console.log("Got error: " + e.message);
+        throw e;
+    });
+    post_req.write(post_data);
+};
+module.exports = GetApi;
