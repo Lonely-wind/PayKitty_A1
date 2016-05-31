7bc538dc09d3bf5c7d2ef484a6cd0658099d60de
changeday <18868103522@163.com>
2016-05-31 13:54:43 +0800

    Add files via upload

--- paykitty.sql
+++ paykitty.sql
@@ -16,13 +16,13 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `dealeraccount`
+-- Table structure for table `DealerAccount`
 --
 
-DROP TABLE IF EXISTS `dealeraccount`;
+DROP TABLE IF EXISTS `DealerAccount`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `dealeraccount` (
+CREATE TABLE `DealerAccount` (
   `DealerNo` char(20) NOT NULL,
   `Name` char(20) DEFAULT NULL,
   `Address` char(100) DEFAULT NULL,
@@ -32,23 +32,23 @@ CREATE TABLE `dealeraccount` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `dealeraccount`
+-- Dumping data for table `DealerAccount`
 --
 
-LOCK TABLES `dealeraccount` WRITE;
-/*!40000 ALTER TABLE `dealeraccount` DISABLE KEYS */;
-INSERT INTO `dealeraccount` VALUES ('224','AAAA','Yuhangtang Road#866','Normal');
-/*!40000 ALTER TABLE `dealeraccount` ENABLE KEYS */;
+LOCK TABLES `DealerAccount` WRITE;
+/*!40000 ALTER TABLE `DealerAccount` DISABLE KEYS */;
+INSERT INTO `DealerAccount` VALUES ('224','AAAA','Yuhangtang Road#866','Normal');
+/*!40000 ALTER TABLE `DealerAccount` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `paymentaccount`
+-- Table structure for table `PaymentAccount`
 --
 
-DROP TABLE IF EXISTS `paymentaccount`;
+DROP TABLE IF EXISTS `PaymentAccount`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `paymentaccount` (
+CREATE TABLE `PaymentAccount` (
   `PaymentNo` char(20) NOT NULL,
   `BankName` char(20) DEFAULT NULL,
   `Address` char(100) DEFAULT NULL,
@@ -58,23 +58,23 @@ CREATE TABLE `paymentaccount` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `paymentaccount`
+-- Dumping data for table `PaymentAccount`
 --
 
-LOCK TABLES `paymentaccount` WRITE;
-/*!40000 ALTER TABLE `paymentaccount` DISABLE KEYS */;
-INSERT INTO `paymentaccount` VALUES ('P1000001','ICBC','6228888888888888','Normal'),('P1000002','ICBC','6228888888888888','Normal'),('P1000003','ICBC','6228888888888888','Normal'),('P1000004','ICBC','6228888888888888','Normal'),('P1000005','ICBC','6228888888888888',' Lock'),('P1000006','ICBC','6228888888888888',' Lock'),('P1000007','ICBC','6228888888888888',' Lock'),('P1000008','ICBC','6228888888888888',' Lock');
-/*!40000 ALTER TABLE `paymentaccount` ENABLE KEYS */;
+LOCK TABLES `PaymentAccount` WRITE;
+/*!40000 ALTER TABLE `PaymentAccount` DISABLE KEYS */;
+INSERT INTO `PaymentAccount` VALUES ('P1000001','ICBC','6228888888888888','Normal'),('P1000002','ICBC','6228888888888888','Normal'),('P1000003','ICBC','6228888888888888','Normal'),('P1000004','ICBC','6228888888888888','Normal'),('P1000005','ICBC','6228888888888888',' Lock'),('P1000006','ICBC','6228888888888888',' Lock'),('P1000007','ICBC','6228888888888888',' Lock'),('P1000008','ICBC','6228888888888888',' Lock');
+/*!40000 ALTER TABLE `PaymentAccount` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `useraccount`
+-- Table structure for table `UserAccount`
 --
 
-DROP TABLE IF EXISTS `useraccount`;
+DROP TABLE IF EXISTS `UserAccount`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `useraccount` (
+CREATE TABLE `UserAccount` (
   `AccountID` int(11) NOT NULL AUTO_INCREMENT,
   `AccountName` char(20) DEFAULT NULL,
   `Password` char(50) DEFAULT NULL,
@@ -90,9 +90,13 @@ CREATE TABLE `useraccount` (
   `Type` int(11) DEFAULT NULL,
   `DealerNo` char(20) DEFAULT NULL,
   `Payment1` char(20) DEFAULT NULL,
+  `Payment1Password` char(50) DEFAULT NULL,
   `Payment2` char(20) DEFAULT NULL,
+  `Payment2Password` char(50) DEFAULT NULL,
   `Payment3` char(20) DEFAULT NULL,
+  `Payment3Password` char(50) DEFAULT NULL,
   `Payment4` char(20) DEFAULT NULL,
+  `Payment4Password` char(50) DEFAULT NULL,
   `Balance` decimal(12,2) DEFAULT NULL,
   `PayPassword` char(50) DEFAULT NULL,
   PRIMARY KEY (`AccountID`),
@@ -103,31 +107,31 @@ CREATE TABLE `useraccount` (
   KEY `Payment2` (`Payment2`),
   KEY `Payment3` (`Payment3`),
   KEY `Payment4` (`Payment4`),
-  CONSTRAINT `useraccount_ibfk_2` FOREIGN KEY (`Payment1`) REFERENCES `paymentaccount` (`PaymentNo`),
-  CONSTRAINT `useraccount_ibfk_3` FOREIGN KEY (`Payment2`) REFERENCES `paymentaccount` (`PaymentNo`),
-  CONSTRAINT `useraccount_ibfk_4` FOREIGN KEY (`Payment3`) REFERENCES `paymentaccount` (`PaymentNo`),
-  CONSTRAINT `useraccount_ibfk_5` FOREIGN KEY (`Payment4`) REFERENCES `paymentaccount` (`PaymentNo`)
+  CONSTRAINT `UserAccount_ibfk_2` FOREIGN KEY (`Payment1`) REFERENCES `PaymentAccount` (`PaymentNo`),
+  CONSTRAINT `UserAccount_ibfk_3` FOREIGN KEY (`Payment2`) REFERENCES `PaymentAccount` (`PaymentNo`),
+  CONSTRAINT `UserAccount_ibfk_4` FOREIGN KEY (`Payment3`) REFERENCES `PaymentAccount` (`PaymentNo`),
+  CONSTRAINT `UserAccount_ibfk_5` FOREIGN KEY (`Payment4`) REFERENCES `PaymentAccount` (`PaymentNo`)
 ) ENGINE=InnoDB AUTO_INCREMENT=227 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `useraccount`
+-- Dumping data for table `UserAccount`
 --
 
-LOCK TABLES `useraccount` WRITE;
-/*!40000 ALTER TABLE `useraccount` DISABLE KEYS */;
-INSERT INTO `useraccount` VALUES (123,'A1','123','$salt','2016-05-18 12:49:27','张三','M','332222199901010101','浙大路38号','13800138004','abc@zju.edu.cn',0,0,NULL,'P1000001','P1000008',NULL,NULL,1234.50,'123'),(124,'A2','123','$salt','2016-05-18 17:27:28','李四','F','332222199901010102','浙大路39号','13777776657','abd@zju.edu.cn',1,0,NULL,'P1000002','P1000007',NULL,NULL,1234.50,'123'),(224,'B2','123','$salt','2016-05-17 18:17:33','赵六','F','432222199901010102','浙大路49号','13800138000','bbd@zju.edu.cn',1,1,NULL,'P1000004','P1000005',NULL,NULL,1234.50,'123');
-/*!40000 ALTER TABLE `useraccount` ENABLE KEYS */;
+LOCK TABLES `UserAccount` WRITE;
+/*!40000 ALTER TABLE `UserAccount` DISABLE KEYS */;
+INSERT INTO `UserAccount` VALUES (123,'A1','123','$salt','2016-05-18 12:49:27','张三','M','332222199901010101','浙大路38号','13800138004','abc@zju.edu.cn',0,0,NULL,'P1000001','123', 'P1000008','123', NULL,NULL,NULL,NULL,1234.50,'123'),(124,'A2','123','$salt','2016-05-18 17:27:28','李四','F','332222199901010102','浙大路39号','13777776657','abd@zju.edu.cn',1,0,NULL,'P1000002', '123','P1000007', '123', NULL,NULL,NULL,NULL,1234.50,'123'),(224,'B2','123','$salt','2016-05-17 18:17:33','赵六','F','432222199901010102','浙大路49号','13800138000','bbd@zju.edu.cn',1,1,NULL,'P1000004','123', 'P1000005','123', NULL,NULL,NULL,NULL,1234.50,'123');
+/*!40000 ALTER TABLE `UserAccount` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `usermessage`
+-- Table structure for table `UserMessage`
 --
 
-DROP TABLE IF EXISTS `usermessage`;
+DROP TABLE IF EXISTS `UserMessage`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `usermessage` (
+CREATE TABLE `UserMessage` (
   `MessageID` int(11) NOT NULL AUTO_INCREMENT,
   `AccountID` int(11) NOT NULL DEFAULT '0',
   `MessageSender` char(20) DEFAULT '系统',
@@ -136,18 +140,18 @@ CREATE TABLE `usermessage` (
   `IsClick` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`MessageID`,`AccountID`),
   KEY `AccountID` (`AccountID`),
-  CONSTRAINT `usermessage_ibfk_1` FOREIGN KEY (`AccountID`) REFERENCES `useraccount` (`AccountID`)
+  CONSTRAINT `UserMessage_ibfk_1` FOREIGN KEY (`AccountID`) REFERENCES `UserAccount` (`AccountID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `usermessage`
+-- Dumping data for table `UserMessage`
 --
 
-LOCK TABLES `usermessage` WRITE;
-/*!40000 ALTER TABLE `usermessage` DISABLE KEYS */;
-INSERT INTO `usermessage` VALUES (1,123,'系统','您购买的飞机票已发货','2016-05-24 06:12:48',0),(2,123,'系统','您购买的汽车票已到杭州','2016-05-24 06:12:48',0),(3,123,'系统','您购买的火车票等待签收','2016-05-24 06:12:48',0),(4,124,'系统','您购买的茶杯已发货','2016-05-24 06:12:48',0),(5,124,'系统','您购买的茶壶已到杭州','2016-05-24 06:12:48',0),(6,124,'系统','您购买的勺子等待签收','2016-05-24 06:12:48',0),(7,224,'系统','您售出的飞机票已送出','2016-05-24 06:12:48',0),(8,224,'系统','您售出的汽车票已到杭州','2016-05-24 06:12:48',0),(9,224,'系统','您售出的火车票等待签收','2016-05-24 06:12:48',0);
-/*!40000 ALTER TABLE `usermessage` ENABLE KEYS */;
+LOCK TABLES `UserMessage` WRITE;
+/*!40000 ALTER TABLE `UserMessage` DISABLE KEYS */;
+INSERT INTO `UserMessage` VALUES (1,123,'系统','您购买的飞机票已发货','2016-05-24 06:12:48',0),(2,123,'系统','您购买的汽车票已到杭州','2016-05-24 06:12:48',0),(3,123,'系统','您购买的火车票等待签收','2016-05-24 06:12:48',0),(4,124,'系统','您购买的茶杯已发货','2016-05-24 06:12:48',0),(5,124,'系统','您购买的茶壶已到杭州','2016-05-24 06:12:48',0),(6,124,'系统','您购买的勺子等待签收','2016-05-24 06:12:48',0),(7,224,'系统','您售出的飞机票已送出','2016-05-24 06:12:48',0),(8,224,'系统','您售出的汽车票已到杭州','2016-05-24 06:12:48',0),(9,224,'系统','您售出的火车票等待签收','2016-05-24 06:12:48',0);
+/*!40000 ALTER TABLE `UserMessage` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
