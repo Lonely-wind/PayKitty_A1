c0c63f8a20d6295181fbed9dd426102771930dac
changeday <18868103522@163.com>
2016-06-01 16:41:49 +0800

    Add files via upload

--- AccountManagement/models/transaction.js
+++ AccountManagement/models/transaction.js
@@ -71,4 +71,50 @@ Transaction.RealGetApi = function RealGetApi(regUrl,  port,callback){
     });
     post_req.end();
 };
+Transaction.GetStatistics = function GetStatistics(data, callback){
+    var thisMonthTransaction={
+        total_account : 0,
+        total_order : 0,
+        order_success : 0,
+        order_fail : 0,
+        order_other : 0  
+    };
+    var totalTransaction={
+        total_account : 0,
+        total_order : 0,
+        order_success : 0,
+        order_fail : 0,
+        order_other : 0        
+    }
+    var nowTime = new Date();
+    var isThisMonth = 0;
+    for (var i in data){
+        var time = new  Date(data[i].orderTime.replace(/(^\s*)|(\s*$)/g, '').replace(/\/|\.|\-/g, "/"));
+        console.log(parseInt(nowTime.getMonth()) + "----------------" +  parseInt(time.getMonth()));
+        console.log(parseInt(nowTime.getMonth()) ==  parseInt(time.getMonth()));
+        if(parseInt(nowTime.getMonth()) ==  parseInt(time.getMonth())){
+            isThisMonth = 1;
+        }
+        else{
+            isThisMonth = 0;
+        }
+        totalTransaction.total_order++;
+        totalTransaction.total_account += parseFloat(data[i].orderAmount);
+        thisMonthTransaction.total_order+= isThisMonth;
+        thisMonthTransaction.total_account +=  isThisMonth * parseFloat(data[i].orderAmount);        
+        if(data[i].orderStatus == "交易成功"){
+            totalTransaction.order_success++;
+            thisMonthTransaction.order_success+= isThisMonth;            
+        }
+        else if(data[i].orderStatus == "交易关闭" || data[i].orderStatus == "已退款"){
+            totalTransaction.order_fail++;
+            thisMonthTransaction.order_fail+= isThisMonth;            
+        }
+        else{
+            totalTransaction.order_other++;
+            thisMonthTransaction.order_other+= isThisMonth;
+        }
+    }
+    callback(thisMonthTransaction, totalTransaction);
+}
 module.exports = Transaction;
